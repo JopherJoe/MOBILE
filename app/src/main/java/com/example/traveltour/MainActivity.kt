0313b8f27a392a8f35de283aa02f94df480aa3bd
Jopher Joe S. Ribo <josi.ribo.up@phinmaed.com>
2023-10-02 16:32:40 +0800

    redesigned t

--- app/src/main/java/com/example/traveltour/MainActivity.kt
+++ app/src/main/java/com/example/traveltour/MainActivity.kt
@@ -2,37 +2,27 @@ package com.example.traveltour
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.traveltour.ui.theme.TRAVELTOURTheme
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            setContentView(R.layout.activity_main)
-            val nextActivityButton = findViewById<Button>(R.id.btnNext)
-            val nextActivityButton2 = findViewById<Button>(R.id.btnSignUp)
 
-            nextActivityButton.setOnClickListener{
-                val intent = Intent(this, MainActivity3::class.java)
+            setContentView(R.layout.activity_main);
+
+
+            val textView2 = findViewById<TextView>(R.id.signup)
+            textView2.setOnClickListener {
+                val intent = Intent (this@MainActivity, MainActivity2::class.java)
                 startActivity(intent)
-            }
-            nextActivityButton2.setOnClickListener{
-                val intent = Intent (this, MainActivity2::class.java)
-                startActivity(intent)
-            }
+
+
         }
     }
 }
--- app/src/main/java/com/example/traveltour/MainActivity2.kt
+++ app/src/main/java/com/example/traveltour/MainActivity2.kt
@@ -1,32 +1,44 @@
 package com.example.traveltour
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
-
-
 class MainActivity2 : ComponentActivity() {
-
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
-            setContentView(R.layout.activity_main2)
-            val cancelButton = findViewById<Button>(R.id.btnCancel)
-            val signUpButton = findViewById<Button>(R.id.btnSignUp2)
+        setContentView(R.layout.activity_main2)
 
-            cancelButton.setOnClickListener{
-                finish()
+        val createAccount = findViewById<Button>(R.id.createaccount)
+        val firstname = findViewById<EditText>(R.id.signup_firstname)
+        val lastname = findViewById<EditText>(R.id.signup_lastname)
+        val email = findViewById<EditText>(R.id.signup_emailaddress)
+        val password = findViewById<EditText>(R.id.signup_password)
+        val contact = findViewById<EditText>(R.id.signup_contactno)
+
+
+        createAccount.setOnClickListener(View.OnClickListener {
+            val firstname = firstname.text.toString()
+            val lastname = lastname.text.toString()
+            val email = email.text.toString()
+            val password = password.text.toString()
+            val contact = contact.text.toString()
+
+            if(firstname.isEmpty() || lastname.isEmpty() || email.isEmpty() || password.isEmpty() || contact.isEmpty()){
+               Toast.makeText(this@MainActivity2, "Please fill out all required fields.", Toast.LENGTH_SHORT).show()
+            }else {
+
+                val intent = Intent(this@MainActivity2, MainActivity::class.java)
+                startActivity((intent))
+                Toast.makeText(this@MainActivity2, "You've successfully created your account.", Toast.LENGTH_SHORT).show()
             }
-            signUpButton.setOnClickListener{
-                val intent = Intent (this, MainActivity3::class.java)
-                startActivity(intent)
-            }
-        }
+        })
+
     }
 }
