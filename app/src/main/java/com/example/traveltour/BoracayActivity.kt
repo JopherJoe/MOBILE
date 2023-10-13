8e5bc0e386f1b33b411b221912ffa3868b6cd33d
Jopher Joe S. Ribo <josi.ribo.up@phinmaed.com>
2023-10-14 06:29:18 +0800

    shesh boracay

---
+++ app/src/main/java/com/example/traveltour/BoracayActivity.kt
@@ -0,0 +1,11 @@
+package com.example.traveltour
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class BoracayActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_boracay)
+    }
+}
